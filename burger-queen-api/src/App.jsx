7b386c83ee4d7162ebe149f0b9d7cacdcf529c9e
camilaseguel <seguel.camilaalejandra@gmail.com>
2023-09-18 18:44:20 -0300

    rutas y primera petición http de login

--- burger-queen-api/src/App.jsx
+++ burger-queen-api/src/App.jsx
@@ -2,14 +2,21 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-import { LogInWaiter } from './LogInWaiter'
+import { Routes, Route } from "react-router-dom"
+import { Home } from './Home'
+import { Login } from './Login'
 
-function App() {
+
+
+export function App() {
   //const [count, setCount] = useState(0)
   return (
     <>
-    <LogInWaiter/>
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/Login" element={< Login />} />
+      </Routes>
+
     </>
   )
 }
-export default App
