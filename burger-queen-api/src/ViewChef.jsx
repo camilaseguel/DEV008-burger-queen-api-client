762cb09b9835bb31548a1decb7979558377fce99
camilaseguel <seguel.camilaalejandra@gmail.com>
2023-10-10 18:48:39 -0300

    Added getOrders(), view orders pending in console

--- burger-queen-api/src/ViewChef.jsx
+++ burger-queen-api/src/ViewChef.jsx
@@ -1,23 +1,34 @@
 import { ProductList } from "./ProductList";
 import { ProductWaiter } from "./ProductWaiter";
+import { getOrders } from "./apiRequests";
+
+import { useState, useEffect } from "react";
 
 export function ViewChef() {
     const [view, setView] = useState([])
 
-    return(
+    useEffect(() => {
+        getOrders().then((result) => {
+            console.log(result)
+            setView(result); // Almacena los resultados en el estado
+        });
+    }, []);
+
+
+    return (
         <>
-        <header className="title-viewchef">
-            <h2>Orders to prepare</h2>
-        </header>
-        <section className="container-list-order">
-            <ul>
-                <li>
+            <header className="title-viewchef">
+                <h2>Orders to prepare</h2>
+            </header>
+            <section className="container-list-order">
+                <ul>
+                    <li>
 
-                </li>
-            </ul>
+                    </li>
+                </ul>
 
-        </section>
-        
+            </section>
+
         </>
     )
 }
