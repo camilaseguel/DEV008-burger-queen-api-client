df7ef9a93429ffd11dca1b27b4423045641c9937
camilaseguel <seguel.camilaalejandra@gmail.com>
2023-10-11 14:45:32 -0300

    viewChef components,ok - without style and checkbox

--- burger-queen-api/src/ViewChef.jsx
+++ burger-queen-api/src/ViewChef.jsx
@@ -5,15 +5,13 @@ import { getOrders } from "./apiRequests";
 import { useState, useEffect } from "react";
 
 export function ViewChef() {
-    const [view, setView] = useState([])
+    const [view, setView] = useState([]);
 
     useEffect(() => {
         getOrders().then((result) => {
-            console.log(result)
             setView(result); // Almacena los resultados en el estado
         });
     }, []);
-
 
     return (
         <>
@@ -21,14 +19,20 @@ export function ViewChef() {
                 <h2>Orders to prepare</h2>
             </header>
             <section className="container-list-order">
-                <ul>
-                    <li>
 
-                    </li>
-                </ul>
+                {view.map((order) => (
 
+                    <div className="order-list" key={order.id}>
+                        Id: {order.id} Client: {order.clientName}  Products: {Object.keys(order.products).map((productId) => (
+                            <div key={productId}>
+                                Product Name: {order.products[productId].product.name}
+                                Quantity: {order.products[productId].qty}
+                            </div>
+                        ))} Status:  {order.status} dateEntry:  {order.dateEntry}
+                    </div>
+
+                ))}
             </section>
-
         </>
-    )
+    );
 }
