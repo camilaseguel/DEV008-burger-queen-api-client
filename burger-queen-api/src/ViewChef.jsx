63645d4bd4df979692c5cd5f8533b1c3778986cb
camilaseguel <seguel.camilaalejandra@gmail.com>
2023-10-11 15:57:04 -0300

    re-login user, sesión había expirado

--- burger-queen-api/src/ViewChef.jsx
+++ burger-queen-api/src/ViewChef.jsx
@@ -1,36 +1,39 @@
+import { ProductList } from "./ProductList";
+import { ProductWaiter } from "./ProductWaiter";
 import { getOrders } from "./apiRequests";
 
 import { useState, useEffect } from "react";
 
 export function ViewChef() {
-    const [view, setView] = useState([]);
+  const [view, setView] = useState([]);
+  console.log(view)
 
-    useEffect(() => {
-        getOrders().then((result) => {
-            setView(result); // Almacena los resultados en el estado
-        });
-    }, []);
+  useEffect(() => {
+    getOrders().then((result) => {
+      setView(result); // Almacena los resultados en el estado
+    });
+  }, []);
 
-    return (
-        <>
-            <header className="title-viewchef">
-                <h2>Orders to prepare</h2>
-            </header>
-            <section className="container-list-order">
+  return (
+    <>
+      <header className="title-viewchef">
+        <h2>Orders to prepare</h2>
+      </header>
+      <section className="container-list-order">
 
-                {view.map((order) => (
+        {view.map((order) => (
 
-                    <div className="order-list" key={order.id}>
+          <div className="order-list" key={order.id}>
+            Id: {order.id} Client: {order.clientName}  Products: {Object.keys(order.products).map((productId) => (
+              <div key={productId}>
+                Product Name: {order.products[productId].product.name}
+                Quantity: {order.products[productId].qty}
+              </div>
+            ))} Status:  {order.status} dateEntry:  {order.dateEntry}
+          </div>
 
-                        Id: {order.id} Client: {order.clientName}  Products: {Object.keys(order.products).map((productId) => (
-                            <div key={productId}>
-                                Product Name: {order.products[productId].product.name}
-                                Quantity: {order.products[productId].qty}
-                            </div>
-                        ))} Status:  {order.status} dateEntry:  {order.dateEntry}
-                    </div>
-                ))}
-            </section>
-        </>
-    );
+        ))}
+      </section>
+    </>
+  );
 }
