ef5b216f994da36709700eca9ee2fd43264bb76e
Dani-dan99 <ldcpdan@gmail.com>
2023-10-12 12:52:34 -0600

    Added style viewChef and checkBox

--- burger-queen-api/src/ViewChef.jsx
+++ burger-queen-api/src/ViewChef.jsx
@@ -1,12 +1,14 @@
-import { ProductList } from "./ProductList";
-import { ProductWaiter } from "./ProductWaiter";
-import { getOrders } from "./apiRequests";
+import { ProductList } from './ProductList';
+import { ProductWaiter } from './ProductWaiter';
+import { getOrders } from './apiRequests';
+import { useState, useEffect } from 'react';
 
-import { useState, useEffect } from "react";
+import './viewChef.css'
+
 
 export function ViewChef() {
   const [view, setView] = useState([]);
-  console.log(view)
+  console.log(view);
 
   useEffect(() => {
     getOrders().then((result) => {
@@ -16,22 +18,25 @@ export function ViewChef() {
 
   return (
     <>
-      <header className="title-viewchef">
+      <header className='title-viewchef'>
         <h2>Orders to prepare</h2>
       </header>
-      <section className="container-list-order">
-
+      <section className='container-list-order'>
         {view.map((order) => (
-
-          <div className="order-list" key={order.id}>
-            Id: {order.id} Client: {order.clientName}  Products: {Object.keys(order.products).map((productId) => (
+          <div className='order-list' key={order.id}>
+            Id: {order.id} Client: {order.clientName} Products:{' '}
+            {Object.keys(order.products).map((productId) => (
               <div key={productId}>
                 Product Name: {order.products[productId].product.name}
                 Quantity: {order.products[productId].qty}
               </div>
-            ))} Status:  {order.status} dateEntry:  {order.dateEntry}
+            ))}{' '}
+            Status: {order.status} dateEntry: {order.dateEntry}
+            <label className='containerBox'>Ready
+            <input className='checkBox' type='checkbox' name='myCheckbox' defaultChecked={false} />
+            <span className='checkmark'></span>
+            </label>
           </div>
-
         ))}
       </section>
     </>
